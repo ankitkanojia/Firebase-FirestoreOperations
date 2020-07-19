72469335e04bd151bc9d6847d32b61a38ed47f8a
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-07-19 13:12:00 +0530

    Integrated Firebase context with GET API call which can able to fetch data from firestore database.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import FireBaseContext from "./firebase-context";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
     }
+  }
+
+  componentDidMount() {
+    // GET method or Fetch data API call from firestore database
+    FireBaseContext().collection("Students")
+      .get()
+      .then(collectionData => {
+        // you will get student data collection in collectionData object
+        console.log('data collection : ', collectionData);
+      }).catch(err => {
+        console.log('error', err.response);
+      });
   }
 
   render() {
