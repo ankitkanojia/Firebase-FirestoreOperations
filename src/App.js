6569571fd5f8601577e5aa5710ad6693176e2874
Ankit Kanojia <ankit.kanojia@devdigital.com>
2020-12-21 20:33:33 +0530

    Handled add method which added the data using Firebase APIs

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-   
     // GET method or Fetch data API call from firestore database
     FireBaseContext().collection("Students")
       .get()
@@ -31,6 +30,21 @@ class App extends Component {
       }).catch(err => {
         // Error or Exception occure
         console.log('error', err.response);
+      });
+  }
+
+  addDetails = (Obj) => {
+    // Add document using delete option from firestore database
+    FireBaseContext().collection("Students")
+      .doc(obj.Id)
+      .set(obj)
+      .then(function () {
+        // Student details addedd in firebase database  
+        console.log('Student details successfully added!');
+      })
+      .catch(function (error) {
+        // Error or Exception occure
+        console.error("Error writing document: ", error);
       });
   }
 
